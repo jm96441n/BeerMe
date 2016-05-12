05e16764eaed05a979f50ef804b35299c9efd110
John Maguire <jmaguire5588@gmail.com>
2016-05-12 01:16:37 -0400

    added full info for brewery index

--- app/assets/javascripts/components/Breweries/Brewery.js.jsx
+++ app/assets/javascripts/components/Breweries/Brewery.js.jsx
@@ -4,6 +4,10 @@ var Brewery = React.createClass({
       <li>
         <div>
           <p>Brewery: {this.props.brewery.name}</p>
+          <p>Street Address: {this.props.brewery.address1},{this.props.brewery.address2}, {this.props.brewery.city}, {this.props.brewery.state}, {this.props.brewery.code}, {this.props.brewery.country}</p>
+          <p>Phone: {this.props.brewery.phone}</p>
+          <p>Website: {this.props.brewery.website}</p>
+          <p>About: {this.props.brewery.description}</p>
         </div>
       </li>
     )
