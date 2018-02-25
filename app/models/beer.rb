e8e673e177b3e56903a50368de259c027bde00f4
John Maguire <jmaguire@mdsol.com>
2018-02-24 19:40:07 -0500

    added category for beer styles

--- app/models/beer.rb
+++ app/models/beer.rb
@@ -1,4 +1,9 @@
 class Beer < ApplicationRecord
   belongs_to :brewery
   belongs_to :beer_style
+
+  # Returns the category for this beer
+  def category
+    beer_style.category
+  end
 end
--- spec/models/beer_spec.rb
+++ spec/models/beer_spec.rb
@@ -1,14 +1,16 @@
 require 'rails_helper'
 
 describe Beer, type: :model do
-  subject(:beer) { Beer.new }
   let(:category) { double('Category') }
   let(:beer_style) { double('BeerStyle', category: category) }
   let(:brewery) { double('Brewery') }
-
-  allow(beer).to receive(:beer_style).and_return(beer_style)
+  subject(:beer) { Beer.new() }
 
   describe '.category' do
+    before do
+      allow(beer).to receive(:beer_style).and_return(beer_style)
+    end
+
     it 'returns the beers category' do
       expect(beer.category).to eql category
     end
