6ae8db022570897d8712231ec9472245f7aaea0e
John Maguire <jmaguire@mdsol.com>
2018-04-11 20:30:06 -0400

    added image to beer page and updated some styling

--- app/javascript/bundles/Beers/components/Beer.tsx
+++ app/javascript/bundles/Beers/components/Beer.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+const beerImage = 'https://s3.amazonaws.com/beer-me/beer-mug.png';
 
 export default class Beer extends React.Component<IBeerProps, {}> {
   constructor(props: IBeerProps) {
@@ -11,33 +12,43 @@ export default class Beer extends React.Component<IBeerProps, {}> {
     let html: any;
     if(this.props.description){
       html = (
-        <div>
-          <p>
-            <strong>Name:</strong> {this.props.name}
-          </p>
-          <p>
-            <strong>ABV%</strong>: {this.props.abv}%
-          </p>
-          <p>
-            <strong>IBU:</strong> {this.props.ibu}
-          </p>
-          <p>
-            <strong>Description:</strong> {this.props.description}
-          </p>
+        <div className='row'>
+          <div className='col-md-4'>
+            <img className="img-thumbnail img-responsive" src={beerImage} />
+          </div>
+          <div className='col-md-8'>
+            <p>
+              <strong>Name:</strong> {this.props.name}
+            </p>
+            <p>
+              <strong>ABV%</strong>: {this.props.abv}%
+            </p>
+            <p>
+              <strong>IBU:</strong> {this.props.ibu}
+            </p>
+            <p>
+              <strong>Description:</strong> {this.props.description}
+            </p>
+          </div>
         </div>
       )
     } else {
       html = (
-        <div>
-          <p>
-            <strong>Name:</strong> {this.props.name}
-          </p>
-          <p>
-            <strong>ABV%</strong>: {this.props.abv}%
-          </p>
-          <p>
-            <strong>IBU:</strong> {this.props.ibu}
-          </p>
+        <div className='row'>
+          <div className='col-md-4'>
+            <img className="img-thumbnail img-responsive" src={beerImage} />
+          </div>
+          <div className='col-md-8'>
+            <p>
+              <strong>Name:</strong> {this.props.name}
+            </p>
+            <p>
+              <strong>ABV%</strong>: {this.props.abv}%
+            </p>
+            <p>
+              <strong>IBU:</strong> {this.props.ibu}
+            </p>
+          </div>
         </div>
       )
     }
@@ -47,7 +58,7 @@ export default class Beer extends React.Component<IBeerProps, {}> {
   render() {
     let html: any = this.getBody()
     return (
-      <div>
+      <div id='beer'>
         { html }
       </div>
     )
